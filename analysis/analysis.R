eb1e20eacec60d3c535dbde9c7f4ea2f22e812b9
tfjaeger <fjaeger@ur.rochester.edu>
2025-02-13 09:58:35 -0500

    initial result plots

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,22 +1,39 @@
 library(tidyverse)
+library(ggstance)
 
 d.study1 <-
-  read_csv("Study1.csv") %>%
+  read_csv("results/model_response_log_likelihoods.csv") %>%
   rename(
-    log_likelihood.perceptual_responses_training_data = test_log_likelihood,
-    log_likelihood.perceptual_responses_test_data = held_out_log_likelihood,
-    log_likelihood.intended_category_test_data = test_log_likelihood,
-    Feature.Type = `Study Type`, Experiment = `Perceptual data`, tau = distance_used, k = similarity_scaling_used) %>%
+    log_likelihood.perceptual_responses_training_data = opt_log_likelihood,
+    log_likelihood.perceptual_responses_test_data = heldout_log_likelihood,
+    log_likelihood.intended_category_test_data = train_log_likelihood,
+    tau = distance_used, k = similarity_scaling_used) %>%
   mutate(
-    cv_fold.phonetic = rep(1:5, 24),
-    cv_fold.perceptual = sort(rep(1:2, 60)),
+    Experiment = gsub("^.*(Natural|Synthetic)$", "\\1", model_number),
+    cv_fold.phonetic = rep(1:5, nrow(.) / 5),
+    cv_fold.perceptual = 1,
     parameters = map2(tau, k, ~ expression(tau == .(.x), k == .(.y))))
 
+# Visualizing fit on heldout data
 d.study1 %>%
+  filter(Experiment == "Natural") %>%
   ggplot(aes(x = factor(k), y = log_likelihood.perceptual_responses_test_data, color = factor(tau))) +
   stat_summary(fun.data = mean_cl_boot, geom = "pointrange", position = position_dodge(.2)) +
   scale_x_discrete(expression(Similarity~scaling~k)) +
   scale_y_continuous("model's log likelihood") +
   scale_color_discrete(expression(Distance~exponent~tau)) +
-  facet_wrap( ~ Experiment, scales = "free_y", labeller = label_both) +
+  facet_grid(features_used ~ Experiment, scales = "free_y", labeller = label_both) +
+  theme_bw() + theme(legend.position = "top")
+
+# Visualizing best weights (w1, w2)
+d.study1 %>%
+  filter(Experiment == "Natural") %>%
+  ggplot(aes(x = weight_2, y = weight_1, color = factor(tau), shape = factor(k))) +
+  geom_point(alpha = .5) +
+  # stat_summary(fun.data = mean_cl_boot, geom = "pointrange", position = position_dodge(.2)) +
+  # stat_summaryh(fun.data = mean_cl_boot, geom = "pointrangeh", position = position_dodge(.2)) +
+  scale_x_continuous("F2 weight") +
+  scale_y_continuous("F1 weight") +
+  scale_color_discrete(expression(Distance~exponent~tau)) +
+  facet_grid(features_used ~ Experiment, scales = "free_y", labeller = label_both) +
   theme_bw() + theme(legend.position = "top")
